c5893aa7c0f5d9afefbbe3437682518ccb46fa79
yqrashawn <namy.19@gmail.com>
2016-09-07 23:38:47 +0800

    add files dir, have a rest, think about configuration file first

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../config.json');
+const S = require('string');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/ask', function(req, res) {
-  console.log(req);
+router.post('/ask', function (req, res) {
+  console.log(req.body);
+  const text = req.body.text;
+  let str;
+
+  S.extendPrototype();
+  str = text.chompLeft('!!!!!').s;
+  S.restorePrototype();
+
+  res.send({
+    text: 'text, this field may accept markdown',
+    attachments: [
+      {
+        title: 'title_1',
+        text: 'attachment_text',
+        color: '#666666',
+        images: [
+          {
+            url: 'http://example.com/index.jpg',
+          },
+          {
+            url: 'http://example.com/index.jpg',
+          },
+        ],
+      },
+    ],
+  });
+
 });
 
-router.get('/ask', function(req, res) {
+router.get('/ask', function (req, res) {
   res.render('index', { title: 'Express' });
   console.log(req.body);
+  res.sendStatus(200);
 });
-
-
-
-
-
 
 module.exports = router;
